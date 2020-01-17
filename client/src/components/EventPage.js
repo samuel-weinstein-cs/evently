c6c5e5b44416a55ae7a9f2b63fd123d743b0d3bb
Maleeha <malehahasan@gmail.com>
2020-01-17 13:48:54 -0500

    CreateEvent component added

--- client/src/components/EventPage.js
+++ client/src/components/EventPage.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 function EventPage(props) {
   return (
     <div>
       <main>
+
         <h3>Display all Events</h3>
         <div className="events-wrapper">
-          {props.events.map(event => (
+          {props.eventApiDataLoaded && props.events.events.map(event => (
             <div className="event">
               <p>Title: {event.title}</p>
               <p>Date: {event.date}</p>
               <p>Location:{event.location} </p>
               <p>Time: {event.time}</p>
               <img src={event.image_url} />
-
             </div>
           ))
           }
         </div>
+        <Link to="/CreateEvent" type="submit" ><h1>Create Event</h1></Link>
       </main>
     </div>
   )

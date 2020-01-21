c888839999ade2c782f1bfc9010b4985a696e513
SirRichardM <richardmoeb@gmail.com>
2020-01-20 19:21:58 -0500

    evenpage 75 percent

--- client/src/components/singleEvent.js
+++ client/src/components/singleEvent.js
@@ -9,7 +9,9 @@ class SingleEvent extends Component {
     super(props);
 
     this.state = {
-
+      attending: false,
+      users: null, 
+        changes: false
 
     }
   }
@@ -37,7 +39,8 @@ class SingleEvent extends Component {
         description: eventDat.description,
         entry: eventDat.entry,
         location: eventDat.location,
-        id: eventDat.id
+      id: eventDat.id,
+      
       
 
 
@@ -68,85 +71,149 @@ class SingleEvent extends Component {
     e.preventDefault();
     updateEvent(event.id, event)
     this.setState({
-        
+        changes: false
     })
   }
 
+  handleAttend = (e) => {
+    this.setState({
+      attending: true
+    })
+  }
+
+  handleNotAttend = (e) => {
+    this.setState({
+      attending: false
+    })
+  }
+  
+  needsUpdate = (e) => {
+    this.setState({
+      changes: true
+    })
+  }
+
+  handleHide = (e) => {
+    this.setState({
+      
+      changes: false
+    })
+  }
 
 
   render() {
     
     return (
       <div>
+        <div className="singleEv">
         <h1>{this.state.title}</h1>
         <p>{this.state.date}</p>
-        <p> Category: {this.state.category}</p>
-        <img src={this.state.image_url} />
-        <p>Description : <br />{this.state.description} </p>
-        <p>Entry Fee: {this.state.entry}</p>
-        <p>Location: {this.state.location}</p>
-        <p>Starts at {this.state.startTime}</p>
-        <p>Ends at {this.state.endTime}</p>
+          <p> <span className="tags">Category: </span> {this.state.category}</p>
+          
+        <img src={this.state.image_url}  className="evenImg" />
+           
+        <p> <span className="tags"> Description : </span>  <br />{this.state.description} </p>
+        <p><span className="tags"> Entry Fee: </span> {this.state.entry}</p>
+          <p>
+          <span className="tags"> Location: </span>  {this.state.location}</p>
+        <p> <span className="tags"> Starts at </span>  {this.state.startTime}</p>
+      <p> <span className="tags"> Ends at </span> {this.state.endTime}</p>
+      
+      
+      <div className="attendBut">
+        {this.state.attending === false 
+          ? 
+          <button className="attendBut" onClick={this.handleAttend}>Attending!</button>
+          :
+          <button className="attendBut" onClick={this.handleNotAttend}> Not attending </button>
+            }
+            </div>
+          
+          <h2>Members Atttending</h2>
+          <div className="attend">
+            {this.state.users === null ?
+              <h2>No Memebers Attending At the Moment</h2>
+              :
+              <h2>RICHARD</h2>}
+          </div>
 
-        <form onSubmit={(e) => this.handleDelete(e, this.state.id)}>
+          {this.state.changes === false ?
+            
+            <button onClick={this.needsUpdate}>Need to make any changes?</button>
+            
+            :
+              
+            <form onSubmit={(e) => this.handleUpdate(e, this.state)}>
+              <button>Hide</button>
+              <br/>
+              <input type="text" name="title" value={this.state.title} placeholder="Name of Event" onChange={this.handleChange} />
+            
+              <div className="submitForm">
+
+                <input
+                  type="text"
+                  name="category"
+                  value={this.state.category}
+                  placeholder="Category of Event"
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="text"
+                  name="entry"
+                  value={this.state.entry}
+                  placeholder="Any entry fees?"
+                  onChange={this.handleChange}
+                />
+
+                <input
+
+                  type="text"
+                  name="description"
+                  value={this.state.description}
+                  placeholder="Event Details"
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="text"
+                  name="date"
+                  value={this.state.date}
+                  placeholder="Date of Event"
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="text"
+                  name="location"
+                  value={this.state.location}
+                  placeholder="Address/Location"
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="text"
+                  name="startTime"
+                  value={this.state.startTime}
+                  placeholder="Start Time"
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="text"
+                  name="endTime"
+                  value={this.state.endTime}
+                  placeholder="End Time"
+                  onChange={this.handleChange}
+                />
+                <input type="submit" />
+              
+              </div>
+       
+            </form>
+          } 
+          
+          <form onSubmit={(e) => this.handleDelete(e, this.state.id)}>
           <input type="submit" value="Delete Event" />
-        </form>
+          </form>
+          
+        </div>
 
-        <form onSubmit={(e) => this.handleUpdate(e, this.state)}>
-          <input type="text" name="title" value={this.state.title} placeholder="Name of Event" onChange={this.handleChange} />
-          <input
-            type="text"
-            name="category"
-            value={this.state.category}
-            placeholder="Category of Event"
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="entry"
-            value={this.state.entry}
-            placeholder="Any entry fees?"
-            onChange={this.handleChange}
-          />
-
-          <input
-
-            type="text"
-            name="description"
-            value={this.state.description}
-            placeholder="Event Details"
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="date"
-            value={this.state.date}
-            placeholder="Date of Event"
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="location"
-            value={this.state.location}
-            placeholder="Address/Location"
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="startTime"
-            value={this.state.startTime}
-            placeholder="Start Time"
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="endTime"
-            value={this.state.endTime}
-            placeholder="End Time"
-            onChange={this.handleChange}
-          />
-          <input type="submit"/>
-        </form>
 
       </div>
     )
